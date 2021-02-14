075042983ad7fb334ccffa0a1d3e254840138f46
mtrimner <matt.trimner@gmail.com>
2021-02-14 11:53:43 -0800

    finished adding context

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -1,6 +1,4 @@
-import React, { useReducer } from 'react';
-
-const BlogContext = React.createContext();
+import createDataContext from './createDataContext';
 
 const blogReducer = (state, action) => {
     switch (action.type) {
@@ -11,16 +9,10 @@ const blogReducer = (state, action) => {
     }
 };
 
-export const BlogProvider = ({ children }) => {
-    const [blogPosts, dispatch] = useReducer(blogReducer, []);
-
-    addBlogPost = () => {
+addBlogPost = (dispatch) => {
+    return () => {
         dispatch({ type: 'add_blogpost'})
-    };
-
-    return <BlogContext.Provider value={{data: blogPosts, addBlogPost}}>
-        {children}
-    </BlogContext.Provider>
+    }
 };
 
-export default BlogContext;
+export const { Context, Provider } = createDataContext(blogReducer, { addBlogPost }, []);
