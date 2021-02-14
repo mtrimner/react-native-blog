19b66d791ebd6a696e0d7f8c2610c52b73bbebd7
mtrimner <matt.trimner@gmail.com>
2021-02-13 16:09:31 -0800

    added useReducer hook

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -1,17 +1,24 @@
-import React, { useState } from 'react';
+import React, { useReducer } from 'react';
 
 const BlogContext = React.createContext();
 
-export const BlogProvider = ({ children }) => {
-    const [blogPosts, setBlogPosts] = useState([]);
-
-    const addBlogPost = () => {
-        setBlogPosts([
-            ...blogPosts, { title: `Blog Post #${blogPosts.length + 1}`}
-        ])
+const blogReducer = (state, action) => {
+    switch (action.type) {
+        case 'add_blogpost':
+            return [...state, {title: `Blog Post #${state.length + 1}`}];
+        default:
+            return state;
     }
+};
 
-    return <BlogContext.Provider value={{data: blogPosts, addBlogPost: addBlogPost}}>
+export const BlogProvider = ({ children }) => {
+    const [blogPosts, dispatch] = useReducer(blogReducer, []);
+
+    addBlogPost = () => {
+        dispatch({ type: 'add_blogpost'})
+    };
+
+    return <BlogContext.Provider value={{data: blogPosts, addBlogPost}}>
         {children}
     </BlogContext.Provider>
 };
